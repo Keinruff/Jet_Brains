5435cf1f6eb4107475b38959f39900e686adc6b8
Sergey Sorokin <KIN-KENSKI@YA.RU>
2022-06-11 04:16:57 +0300

    lesson4

--- Lesson_2.py
+++ Lesson_2.py
@@ -1,10 +1,25 @@
-print(f'А теперь давай поиграем, тема СПОРТ , Планируем наши рекорды !!!')
-first_rez = int(input('Введите ваш текущий результат  (дистанция в километрах): '))
-last_rez = int(input('Какую цель вы ставите пееред собой? (дистанция в километрах): '))
-if first_rez >= last_rez:
-    print(f'Вы меня не обманите вы достигли  своей цели')
-day_rez = 1
-while first_rez <= last_rez:
-    first_rez = first_rez + first_rez * 0.1
-    day_rez += 1
-print(f'На {day_rez}  день вы достигните своего результата, в этот день вы пробежите {first_rez} киломтеров')
+# section 1
+
+
+li = ['fio', 1, ['s', 'f']]
+print(type(li[0]))
+print(type(li[2]))
+
+# section 2
+
+li = input('Введите строку через пробел').split()
+if len(li) % 2 == 0:
+    for li[i] in range(len(li)):
+
+else:
+    f = (len(li)) - 1
+for i in f:
+    k = li[i]
+    li
+
+
+print (f)
+# section 3
+# section 4
+# section 5
+# section 6
--- lesson_4.py
+++ lesson_4.py
@@ -1,13 +1,80 @@
 
+# # section 1
+#
+#
+# from sys import argv
+#
+#
+# def zp(time, s, p):
+#     rez = (time * s) + p
+#     return rez
+#
+#
+# a1, a2, a3 = map(int, argv[1:])
+#
+# print('section 1: ', zp(a1, a2, a3))
 
-from sys import argv
+
+# section 2
+
+li = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
+new = [li[i] for i in range(len(li)) if li[i] > li[i-1]]
+print('section 2: ', new)
 
 
-def zp(time, s, p):
-    rez = (time * s) + p
-    return rez
+# section 3
 
 
-a1, a2, a3 = map(int, argv[1:])
+print('section 3: ', [i for i in range(20, 240) if i % 20 == 0 or i % 21 == 0])
 
-print(zp(a1, a2, a3))
+
+# section 4
+
+li = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
+new = [el for el in set(li) if li.count(el) == 1]
+new2 = [li[i] for i in range(len(li)) if li.count(li[i]) == 1]
+print('section 4: ', new)
+print('section 4: ', new2)
+
+
+# section 5
+
+from functools import reduce
+
+
+li = [i for i in range(100, 1001) if i % 2 == 0]
+new = reduce(lambda x, y: x * y, li)
+print('section 5: ', new)
+
+
+# section 6
+
+
+from itertools import count
+print('section 6: ')
+for el in count(3):
+    if el > 10:
+        break
+    else:
+        print(el)
+print('*' * 50)
+
+from itertools import cycle
+i = 0
+li = [2, 7, 23, 1, 44]
+fl = []
+for el in cycle(li):
+    if i > 10:
+        break
+    print(el)
+    fl.append(el)
+    i += 1
+print(fl)
+
+# section 7
+
+n = 5
+
+
+
+
