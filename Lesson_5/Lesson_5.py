51f48a96731c157ce82ee81d1bfa10161c8028a9
Sergey Sorokin <KIN-KENSKI@YA.RU>
2022-06-28 18:48:34 +0300

    lesson5 с4 done

--- Lesson_5/Lesson_5.py
+++ Lesson_5/Lesson_5.py
@@ -21,7 +21,7 @@
 #              break
 
 
-# Chapter2
+# # Chapter2
 # with open(r'Chapter_2.txt', 'r', encoding='utf-8') as file:
 #     text = file.readlines()
 #     print(text)
@@ -30,14 +30,39 @@
 #         print(f'Строка №{i} содержит {sr} слов.')
 
 
-# Chapter3
-with open(r'Chapter_3.txt', 'r', encoding='utf-8') as file:
-    summ = 0
-    count = 0
-    for i in file:
-        summ += float(i.split()[1])
-        count += 1
-        if float(i.split()[1]) < 20000:
-            print(i.split()[0])
-    avg = summ/count
-    print(f'Седний доход: {round(avg,2)}')
+# # Chapter3
+# with open(r'Chapter_3.txt', 'r', encoding='utf-8') as file:
+#     summ = 0
+#     count = 0
+#     for i in file:
+#         summ += float(i.split()[1])
+#         count += 1
+#         if float(i.split()[1]) < 20000:
+#             print(i.split()[0])
+#     avg = summ/count
+#     print(f'Седний доход: {round(avg,2)}')
+
+
+# # Chapter4
+di = {    'Zero' :'Ноль'
+        , 'One'  :'Один'
+        , 'Two'  :'Два'
+        , 'Three' :'Три'
+        , 'Four' :'Четыре'
+        , 'Five' :'Пять'
+        , 'Six'  :'Шесть'
+        , 'Seven':'Семь'
+        , 'Eight':'Восемь'
+        , 'Nine' :'Восемь'
+
+       }
+
+with open(r'Chapter_4.txt', 'r', encoding='utf-8') as file_r:
+    with open(r'Chapter_4_1.txt', 'w', encoding='utf-8') as file_w:
+        for l in file_r:
+            print(l.split())
+            text = (l.replace(l.split()[0], di.get(l.split()[0])))
+            print(text)
+            file_w.writelines(text)
+
+
