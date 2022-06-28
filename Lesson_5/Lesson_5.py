58f2ea175ed11729ab284d87701bb3862f000ae3
Sergey Sorokin <KIN-KENSKI@YA.RU>
2022-06-28 18:12:59 +0300

    lesson5 с2 done

--- Lesson_5/Lesson_5.py
+++ Lesson_5/Lesson_5.py
@@ -1,21 +1,33 @@
-# Chapter1
-import os
+# # Chapter1
+# import os
+#
+# print('Удаление файла в дирректории.')
+# try:
+#     os.remove("Chapter_1.txt")
+# except:
+#     print('Файл не найден.')
+# print('Созадние новго файла.')
+#
+# f = 0
+# with open(r'Chapter_1.txt', 'w', encoding='utf-8') as file:
+#      print('Запущен цикл ввода данных в файл, для выхода введите  для выхода нажмите Enter в пустой строке')
+#
+#      while True:
+#          f += 1
+#          text = input(f'Введите текст {f} строки: ')
+#          file.writelines(text + '\n')
+#
+#          if text == '':
+#              break
 
-print('Удаление файла в дирректории.')
-try:
-    os.remove("Chapter_1.txt")
-except:
-    print('Файл не найден.')
-print('Созадние новго файла.')
 
-f = 0
-with open(r'Chapter_1.txt', 'w', encoding='utf-8') as file:
-     print('Запущен цикл ввода данных в файл, для выхода введите  для выхода нажмите Enter в пустой строке')
+# Chapter2
+with open(r'Chapter_2.txt', 'r', encoding='utf-8') as file:
+    text = file.readlines()
+    print(text)
+    for i, l in enumerate(text, 1):
+        sr = len(l.split())
+        print(f'Строка №{i} содержит {sr} слов.')
 
-     while True:
-         f += 1
-         text = input(f'Введите текст {f} строки: ')
-         file.writelines(text + '\n')
 
-         if text == '':
-             break
+
