1259e0490b9b1f40a0a06de64ae2ce96803fbed8
Sergey Sorokin <KIN-KENSKI@YA.RU>
2022-06-28 18:23:17 +0300

    lesson5 с3 done

--- Lesson_5/Lesson_5.py
+++ Lesson_5/Lesson_5.py
@@ -22,12 +22,22 @@
 
 
 # Chapter2
-with open(r'Chapter_2.txt', 'r', encoding='utf-8') as file:
-    text = file.readlines()
-    print(text)
-    for i, l in enumerate(text, 1):
-        sr = len(l.split())
-        print(f'Строка №{i} содержит {sr} слов.')
+# with open(r'Chapter_2.txt', 'r', encoding='utf-8') as file:
+#     text = file.readlines()
+#     print(text)
+#     for i, l in enumerate(text, 1):
+#         sr = len(l.split())
+#         print(f'Строка №{i} содержит {sr} слов.')
 
 
-
+# Chapter3
+with open(r'Chapter_3.txt', 'r', encoding='utf-8') as file:
+    summ = 0
+    count = 0
+    for i in file:
+        summ += float(i.split()[1])
+        count += 1
+        if float(i.split()[1]) < 20000:
+            print(i.split()[0])
+    avg = summ/count
+    print(f'Седний доход: {round(avg,2)}')
